73b3a0085f5fa22c043f9b2146550cc1c352bb8b
hakbum <hhbhong@gmail.com>
2021-04-11 23:53:42 +0900

    서울에서 김서방 찾기 문제 추가

---
+++ programmers/level1/210411-서울에서김서방찾기.js
@@ -0,0 +1,8 @@
+/*
+ * 코딩테스트 연습 > 연습문제 > 서울에서 김서방 찾기
+ * https://programmers.co.kr/learn/courses/30/lessons/12919?language=javascript
+ * Level1
+ */
+function solution(seoul) {
+  return `김서방은 ${seoul.indexOf('Kim')}에 있다`;
+}
